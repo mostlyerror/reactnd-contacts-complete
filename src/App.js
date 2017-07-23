088b8ae3994a29905246825a62167c6a3da10885
Benjamin Poon <benjamintpoon@gmail.com>
2017-07-23 00:41:49 -0500

    creating contacts!

--- src/App.js
+++ src/App.js
@@ -21,16 +21,32 @@ class App extends Component {
     }))
   }
 
+  createContact(contact) {
+    console.log(contact)
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/"  render={() => (
-            <ListContacts
-              contacts={this.state.contacts}
-              onDeleteContact={this.removeContact}
-            />
-          )}/>
-        <Route path="/create" component={CreateContact}/>
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )}/>
+        <Route path="/create" render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
